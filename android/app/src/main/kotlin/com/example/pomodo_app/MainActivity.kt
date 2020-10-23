ff393efebfae92de9c7da96c9586141f000c598b
SampatPolireddi <sampat.polireddi@gmail.com>
2020-10-23 12:06:09 +0530

    pg1

---
+++ android/app/src/main/kotlin/com/example/pomodo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pomodo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,100 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.red[200], Colors.red[300], Colors.red],
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+            Column(
+              children:<Widget>[
+
+                //icon
+                Row(
+                  children: <Widget>[
+                    Container(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 100, left: 85, bottom: 100, right: 100),
+                        child: Image(
+                          image: AssetImage('assets/images/coder.png'),
+                          width: 200,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+
+                //text
+                Padding(
+                  padding: const EdgeInsets.only(top:10, left: 10, right:10),
+                  child: Container(
+                    child: Text(
+                      'Until we can manage time, we can', 
+                      style: GoogleFonts.cabin(fontSize: 20),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5,),
+                Padding(
+                   padding: const EdgeInsets.only(left: 10, right:10),
+                  child: Container(
+                    child: Text(
+                      'manage nothing else.',
+                      style: GoogleFonts.cabin(fontSize: 20)
+                      ),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                ),
+
+                // button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:<Widget>[
+                    Container(
+                      child: RaisedButton(
+                        elevation: 5,
+                        onPressed: () {},
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Continue",
+                            style: GoogleFonts.cabin(fontSize:18) ,
+                            ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
